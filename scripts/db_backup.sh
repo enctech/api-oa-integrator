1dcbafd811b239ed197ad5aa56b2bba7b7718efd
yusuf <myusufrosman@gmail.com>
2023-12-14 21:59:54 +0800

    wip: db backup

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -20,12 +20,14 @@ docker exec $container_name pg_dump -U $username -d $database > $backup_path/bac
 # Compress the backup file to save space
 tar -czvf backup_$timestamp.sql.tar.gz $backup_path/backup_$timestamp.sql
 
+rm -rf $backup_path/backup_*.sql
+
 # Count the number of existing backups
-backup_count=$(ls -1 $backup_path | grep -c "^backup_.*\.sql$")
+backup_count=$(ls -1 $backup_path | grep -c "^backup_.*\.sql.tar.gz$")
 
 # Delete the oldest backup if the number exceeds the maximum
 if [ $backup_count -gt $max_backups ]; then
     oldest_backup=$(ls -1t $backup_path | grep "^backup_.*\.sql$" | tail -n 1)
-    rm $backup_path/$oldest_backup
+    rm $backup_path/$oldest_backup.tar.gz
 fi
 
