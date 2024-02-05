e2e850221901b0c635bf933c0dcd837e79ef170c
yusuf <myusufrosman@gmail.com>
2024-02-05 23:56:07 +0800

    wip: add csr

--- scripts/update_cert.sh
+++ scripts/update_cert.sh
@@ -5,12 +5,14 @@ PROJECT_DIR="/home/tng/dev/api-oa-integrator"
 CERT_DIR="$PROJECT_DIR/cert"
 PRIVATE_KEY="$CERT_DIR/private-key.pem"
 CERTIFICATE="$CERT_DIR/certificate.pem"
+CSR="$CERT_DIR/csr.pem"
 
 rm -rf $CERT_DIR
 mkdir -p $CERT_DIR
 # Generate a new private key and certificate
-openssl genpkey -algorithm RSA -out $PRIVATE_KEY -pkeyopt rsa_keygen_bits:2048
-openssl req -new -x509 -days 365 -key $PRIVATE_KEY -out $CERTIFICATE -subj "/C=US/ST=State/L=City/O=Organization/OU=Unit/CN=example.com"
+openssl genpkey -algorithm RSA -out $PRIVATE_KEY
+openssl req -new -x509 -days 365 -key $PRIVATE_KEY -out $CERTIFICATE -subj "/C=MY/ST=KL/L=KL/O=Enctech Services Sdn Bhd/OU=OA/CN=OA/emailAddress=zamri@enctechgroup.com"
+openssl req -new -key $PRIVATE_KEY -out $CSR -subj "/C=MY/ST=KL/L=KL/O=Enctech Services Sdn Bhd/OU=OA/CN=OA/emailAddress=zamri@enctechgroup.com"
 
 cd $PROJECT_DIR || true
 
